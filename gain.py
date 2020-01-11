4378aecb75fa9a98bd7e5a453bc32abd39f1617a
Lucas Bodin <56450683+lucasbd11@users.noreply.github.com>
2020-01-11 17:29:49 +0100

    Create gain.py

---
+++ gain.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def create_log(max):
+    list_x = []
+    
+    for i in range(0,max+1):
+        for dec in range(0,9):
+            list_x.append(10**i+dec*10**i)
+    return list_x
+
+w0 = 1119.8
+h0 = 1/2
+
+fct = lambda w: 20*np.log(h0)-10*np.log(1+(w**2)/w0)
+
+y = [fct(i) for i in create_log(3)]
+
+
+plt.plot(create_log(3),y)
+plt.xscale("log")
+plt.show()
